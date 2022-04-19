566ec526961aced76c7f4b5d7d3d92696d0c7ce4
Armando Aranda <arandaae19@gmail.com>
2022-04-19 10:09:57 -0500

    added second commit

--- GitIntro_HelloWorld/GitIntro_HelloWorld/Program.cs
+++ GitIntro_HelloWorld/GitIntro_HelloWorld/Program.cs
@@ -7,6 +7,8 @@ namespace GitIntro_HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+
         }
     }
 }
